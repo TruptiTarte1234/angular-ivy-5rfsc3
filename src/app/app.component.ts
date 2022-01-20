f0dd74c15231ed689f1b735f0f8c53830bfacbfe
TruptiTarte1234 <tarte.trupti@gmail.com>
2022-01-20 23:14:32 +0530

    1 file modified

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -6,5 +6,5 @@ import { Component, VERSION } from '@angular/core';
   styleUrls: [ './app.component.css' ]
 })
 export class AppComponent  {
-  name = 'Angular TTT fdgdgdfg' + VERSION.major;
+  name = 'Angular TTT yyyy' + VERSION.major;
 }
