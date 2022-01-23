20955977c81234d97cf7a47d53f5b1958a780815
TruptiTarte1234 <tarte.trupti@gmail.com>
2022-01-23 08:52:11 +0530

    5 files modified, 3 added

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -5,10 +5,16 @@ import { FormsModule } from '@angular/forms';
 import { AppComponent } from './app.component';
 import { HelloComponent } from './hello.component';
 import { InfoPanelComponent } from './info-panel/info-panel.component';
+import { MainContentComponent } from './main-content/main-content.component';
 
 @NgModule({
-  imports:      [ BrowserModule, FormsModule ],
-  declarations: [ AppComponent, HelloComponent, InfoPanelComponent ],
-  bootstrap:    [ AppComponent ]
+  imports: [BrowserModule, FormsModule],
+  declarations: [
+    AppComponent,
+    HelloComponent,
+    InfoPanelComponent,
+    MainContentComponent,
+  ],
+  bootstrap: [AppComponent],
 })
-export class AppModule { }
+export class AppModule {}
